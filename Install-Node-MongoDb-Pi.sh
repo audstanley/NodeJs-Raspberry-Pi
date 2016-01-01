405f4c883fef90ee7b54679a81e538d3db5ff7ec
audstanley <audstanley@gmail.com>
2016-01-01 14:01:07 -0800

    .

--- Install-Node-MongoDb-Pi.sh
+++ Install-Node-MongoDb-Pi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #creates directory for downloads, and downloads node 4.2.4
 cd ~/ && mkdir temp && cd temp && wget https://nodejs.org/dist/latest/node-v4.2.4-linux-armv7l.tar.gz;
-tar -xzf node-v4.2.4-linux-armv7l.tar.gz
+tar -xzf node-v4.2.4-linux-armv7l.tar.gz;
 cd node-v4.0.0-linux-armv6l;
 ./configure;
 make;
@@ -17,4 +17,4 @@ sudo apt-get install -y scons build-essential libboost-filesystem-dev libboost-p
 cd mongo-nonx86;
 scons;
 sudo scons --prefix=/opt/mongo install;
-#This whole process should take many hours.  Go get a coffee, or do this before going to bed.
+#This whole process should take many hours.  Go get a coffee, or do this before going to bed. 
