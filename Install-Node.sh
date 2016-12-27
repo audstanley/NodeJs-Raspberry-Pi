6856bd5e9cac0dfdaeeb140ed495c21d194be04d
Richard Stanley <audstanley@gmail.com>
2016-12-27 12:53:41 -0800

    Update Install-Node.sh

--- Install-Node.sh
+++ Install-Node.sh
@@ -13,7 +13,9 @@ tar -xzf $LINKTONODE;
 #Remove the tar after extracing it.
 rm $LINKTONODE;
 #remove older version of node:
-rm -R /opt/nodejs/* && rmdir /opt/nodejs/
+rm -R /opt/nodejs/* && rmdir /opt/nodejs/ 
+#remove symlinks
+rm /usr/bin/node /usr/sbin/node /sbin/node /sbin/node /usr/local/bin/node /usr/bin/npm /usr/sbin/npm /sbin/npm /usr/local/bin/npm;
 #This next line will copy Node over to the appropriate folder.
 mv /root/tempNode/$NODEFOLDER /opt/nodejs/;
 #This line will remove the nodeJs tar we downloaded.
