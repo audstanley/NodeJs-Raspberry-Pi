82d57f40671b90837156d9dac985794384036251
Richard Stanley <audstanley@gmail.com>
2016-12-27 12:08:28 -0800

    Update Install-Node-MongoDb-Pi.sh

--- Install-Node-MongoDb-Pi.sh
+++ Install-Node-MongoDb-Pi.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
+# curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
+LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//')
+NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//')
 #Shell script created by @audstanley
-#Special Thanks to Willi Thiel @ https://ni-c.github.io/heimcontrol.js/get-started.html
-#This will work specifically for a RaspberryPi 2 ARM7.
-#Willi's code installed a old version of NodeJS
-#First, Installation of git.
-sudo apt-get -y install git;
-#Next, Creates directory for downloads, and downloads node 7.2.1
-cd ~/ && mkdir temp && cd temp && wget https://nodejs.org/dist/latest-v7.x/node-v7.2.1-linux-armv7l.tar.gz;
-tar -xzf node-v7.2.1-linux-armv7l.tar.gz;
+#This will work specifically for a RaspberryPi 2/3 ARM7.
+#Next, Creates directory for downloads, and downloads node 7.x
+cd ~/ && mkdir temp && cd temp && wget https://nodejs.org/dist/latest-v7.x/$LINKTONODE
+tar -xzf $LINKTONODE;
 #Remove the tar after extracing it.
-sudo rm node-v7.2.1-linux-armv7l.tar.gz;
+sudo rm $LINKTONODE;
 #This next line will copy Node over to the appropriate folder.
-sudo mv node-v7.2.1-linux-armv7l/ /opt/nodejs/;
+sudo mv $NODEFOLDER /opt/nodejs/;
 #This line will remove the nodeJs we downloaded.
-sudo rm -R node-v7.2.1-linux-armv7l.tar.gz/* && sudo rmdir node-v7.2.1-linux-armv7l.tar.gz/;
+sudo rm -R $LINKTONODE/* && sudo rmdir $LINKTONODE/;
 #Create symlinks to node && npm
 sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
 sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
@@ -23,38 +22,3 @@ sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
 sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
 sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
 sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
-#Installation of MongoDB by downloading binaries, and compiling.
-git clone git://github.com/RickP/mongopi.git;
-#Download the dependencies to install mongoDB on R-Pi2
-sudo apt-get install -y scons build-essential libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libboost-all-dev;
-cd mongopi;
-#SCons will build the binaries appropriate to the Pi.
-scons;
-#Here is the install of MongoDB wih SCons.
-sudo scons --prefix=/opt/mongo install;
-#SCons -c cleans up unwanted files.
-scons -c;
-#Creation of a path variable into the envoirnment
-sudo su;
-echo "PATH=$PATH:/opt/mongo/bin/" >> /etc/enviornment;
-echo "export PATH" >> /etc/enviornment;
-su pi;
-#simlink mongo for shell access:
-sudo ln -s /opt/mongo/bin/mongo /usr/bin/mongo;
-sudo ln -s /opt/mongo/bin/mongoinfo /usr/bin/mongoinfo;
-#add a new mongodb user
-sudo useradd mongodb;
-sudo mkdir /var/lib/mongodb;
-sudo chown mongodb:mongodb /var/lib/mongodb;
-sudo mkdir /etc/mongodb/;
-sudo sh -c 'echo "dbpath=/var/lib/mongodb" > /etc/mongodb/mongodb.conf';
-cd /etc/init.d;
-#grab the mongodb.sh from a github gist
-sudo wget -O mongodb https://gist.github.com/ni-c/fd4df404bda6e87fb718/raw/36d45897cd943fbd6d071c096eb4b71b37d0fcbb/mongodb.sh;
-#make the shell script exicutable.
-sudo chmod +x mongodb;
-#Update rc.d for the mongodb.sh to run on startup
-sudo update-rc.d mongodb defaults;
-#finally, start mongoDB after all the installations are finished.
-sudo service mongodb start;
-#This whole process should take six hours on the raspberry pi 2.  Go get a coffee, or do this before going to bed.
