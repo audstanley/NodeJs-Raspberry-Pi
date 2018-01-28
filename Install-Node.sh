61c1e430cbf7da419890b0cfcebcc49c65a6b331
gahujipo <github@fischnaller.bz>
2018-01-28 20:25:34 +0100

    removed sudo because script already needs to be executed as root

--- Install-Node.sh
+++ Install-Node.sh
@@ -21,11 +21,11 @@ mv ~/tempNode/$NODEFOLDER /opt/nodejs/;
 #This line will remove the nodeJs tar we downloaded.
 rm -R -f ~/tempNode/$LINKTONODE/;
 #Create symlinks to node && npm
-sudo ln -s /opt/nodejs/bin/node /usr/bin/node; sudo ln -s /opt/nodejs/bin/node /usr/sbin/node; 
-sudo ln -s /opt/nodejs/bin/node /sbin/node; sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node; 
-sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm; 
-sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm; sudo ln -s /opt/nodejs/bin/npm /sbin/npm; 
-sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm; 
+ln -s /opt/nodejs/bin/node /usr/bin/node; ln -s /opt/nodejs/bin/node /usr/sbin/node; 
+ln -s /opt/nodejs/bin/node /sbin/node; ln -s /opt/nodejs/bin/node /usr/local/bin/node; 
+ln -s /opt/nodejs/bin/npm /usr/bin/npm; 
+ln -s /opt/nodejs/bin/npm /usr/sbin/npm; ln -s /opt/nodejs/bin/npm /sbin/npm; 
+ln -s /opt/nodejs/bin/npm /usr/local/bin/npm; 
 rm -R -f /root/tempNode/;
 #su pi;
 #cd ~/ && rm -R NodeJs-Raspberry-Pi-Arm9/;
