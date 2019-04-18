a4fab84a12664a4e8a0899b4b42de80c5bd1ef79
Tim Malich <tim.malich@gmail.com>
2019-04-18 18:37:20 +0200

    fix version collision

--- Install-Node.sh
+++ Install-Node.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # written by Richard Stanley (audstanley);
 PICHIP=$(uname -m);
+MAJOR_NODEJS_VERSION=v11
 if [ "$EUID" -ne 0 ]
         then echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
-else LINKTONODE=$(curl -sG https://nodejs.org/dist/latest-v11.x/ | awk '{print $2}' | grep -P 'href=\"node-v11\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
+else LINKTONODE=$(curl -sG https://nodejs.org/dist/latest-v11.x/ | awk '{print $2}' | grep -P 'href=\"node-'${MAJOR_NODEJS_VERSION}'\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
 # curl -G https://nodejs.org/dist/latest-v11.x/ | awk '{print $2}' | grep -P 'href=\"node-v11\.\d{1,}\.\d{1,}-linux-armv10l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
 NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//');
 #Next, Creates directory for downloads, and downloads node 8.x
-cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-v10.x/$LINKTONODE;
+cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-${MAJOR_NODEJS_VERSION}.x/$LINKTONODE;
 tar -xzf $LINKTONODE;
 #Remove the tar after extracing it.
 rm $LINKTONODE;
