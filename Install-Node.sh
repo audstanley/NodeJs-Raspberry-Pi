a287370b90b3468dd3c1d6436c163da6c0d3fa39
Adam Kumpf <akumpf@gmail.com>
2019-10-15 19:20:04 -0400

    If statement ends too early?
    Moving the `fi` to after the else fixed this for me.

--- Install-Node.sh
+++ Install-Node.sh
@@ -9,7 +9,7 @@ else cd /bin/;
 rm node-install;
 wget https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/node-install;
 chmod +x node-install;
-if [ $PICHIP = "armv6l" ]; then /bin/node-install -v 11; fi; # because there is no ARM6 build for version 12.
+if [ $PICHIP = "armv6l" ]; then /bin/node-install -v 11; # because there is no ARM6 build for version 12.
 else /bin/node-install -a;
-
+fi
 fi
