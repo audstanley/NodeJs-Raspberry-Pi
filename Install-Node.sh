036af002cf04c0dde4de73dd436d6ad3f10ae001
Ste Jervis <ste.jervis@gmail.com>
2019-10-21 20:28:33 -0500

    Update Install-Node.sh
    fi terminating too early

--- Install-Node.sh
+++ Install-Node.sh
@@ -2,14 +2,20 @@
 # written by Richard Stanley (audstanley);
 PICHIP=$(uname -m);
 MAJOR_NODEJS_VERSION=v12
-if [ "$EUID" -ne 0 ]
-        then echo "You need to install as root by using sudo ./Install-Node.sh";
+if [ "$EUID" -ne 0 ] 
+then 
+        echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
-else cd /bin/;
-rm node-install;
-wget https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/node-install;
-chmod +x node-install;
-if [ $PICHIP = "armv6l" ]; then /bin/node-install -v 11; fi; # because there is no ARM6 build for version 12.
-else /bin/node-install -a;
+else 
+        cd /bin/;
+        rm node-install;
+        wget https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/node-install;
+        chmod +x node-install;
 
+        if [ $PICHIP = "armv6l" ] 
+        then 
+                /bin/node-install -v 11; 
+        else 
+                /bin/node-install -a;
+        fi # because there is no ARM6 build for version 12.
 fi
