9cb52293d38a8066de5cfe31fb710e8cb4a35348
Richard Stanley <audstanley@gmail.com>
2019-10-23 11:08:55 -0700

    NodeJs v13

--- Install-Node.sh
+++ Install-Node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # written by Richard Stanley (audstanley);
 PICHIP=$(uname -m);
-MAJOR_NODEJS_VERSION=v12
+MAJOR_NODEJS_VERSION=v13
 if [ "$EUID" -ne 0 ]
         then echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
@@ -9,7 +9,7 @@ else cd /bin/;
 rm node-install;
 wget https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/node-install;
 chmod +x node-install;
-if [ $PICHIP = "armv6l" ]; then /bin/node-install -v 11; fi; # because there is no ARM6 build for version 12.
+if [ $PICHIP = "armv6l" ]; then /bin/node-install -v 11; fi; # because there is no ARM6 build for version 12 or 13.
 else /bin/node-install -a;
 
 fi
