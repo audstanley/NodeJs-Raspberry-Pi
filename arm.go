95719400c9c36c010027e955c6606fd77179b044
Richard Stanley <audstanley@gmail.com>
2020-07-29 20:18:20 +0000

    added arrayToString function, which handels ascii differently on different archetectures

---
+++ arm.go
@@ -0,0 +1,17 @@
+// +build arm
+
+package main
+
+import "strings"
+
+func arrayToString(x [65]uint8) string {
+	var buf [65]byte
+	for i, b := range x {
+		buf[i] = byte(b)
+	}
+	str := string(buf[:])
+	if i := strings.Index(str, "\x00"); i != -1 {
+		str = str[:i]
+	}
+	return str
+}
