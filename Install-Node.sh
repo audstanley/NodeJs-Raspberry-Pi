04549833a450d8628304e5781a45c5393428a675
Richard Stanley <audstanley@gmail.com>
2020-11-06 22:29:58 +0000

    Update Install-Node.sh

--- Install-Node.sh
+++ Install-Node.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # written by Richard Stanley (audstanley);
 PICHIP=$(uname -m);
+if [ "$PICHIP" == "aarch64" ]
+then
+        PICHIP="arm64";
+fi
 if [ "$EUID" -ne 0 ] 
 then 
         echo "You need to install as root by using sudo ./Install-Node.sh";
