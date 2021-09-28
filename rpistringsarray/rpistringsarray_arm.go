1477a5738b542997635f295e7ffa94d85570d46d
Richard Stanley <audstanley@gmail.com>
2021-09-28 11:23:41 -0700

    added the string array dependency

---
+++ rpistringsarray/rpistringsarray_arm.go
@@ -0,0 +1,18 @@
+//go:build arm
+// +build arm
+
+package rpistringsarray
+
+import "strings"
+
+func ArrayToString(x [65]uint8) string {
+	var buf [65]byte
+	for i, b := range x {
+		buf[i] = byte(b)
+	}
+	str := string(buf[:])
+	if i := strings.Index(str, "\x00"); i != -1 {
+		str = str[:i]
+	}
+	return str
+}
